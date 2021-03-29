33b0b031da7d5dd8b0d5783b140468c4b4f804c7
Joe Malone <joe.d.malone@gmail.com>
2021-03-29 18:26:18 -0500

    Switch statement added to inquirer prompt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const inquirer = require('inquirer');
+const q = require('inquirer');
 const table = require('console.table');
 require('dotenv').config();
 
@@ -19,7 +19,7 @@ const connection = mysql.createConnection({
 
 
 
-const getDepartments = () => {
+const viewDepts = () => {
     connection.query('SELECT * FROM departments', (err, res) => {
     if (err) throw err;
     // Log all results of the SELECT statement
@@ -28,7 +28,58 @@ const getDepartments = () => {
   });
 }
 
+function askQuestions() {
+
+  let questions = {
+    type: 'list',
+    name: 'objective',
+    message: 'What would you like to do?',
+    choices: [ 
+      "Add a Department to the Database", 
+      "Add a Role to the Database", 
+      "Add an Employee to the Database", 
+      "View All Departments in the Database", 
+      "View All Roles in the Database", 
+      "View All Employees in the Database",
+      "Change the Roles of an Employee", 
+      "QUIT"
+    ]
+  }
+
+  q.prompt(questions).then((answers) => {
+    switch(answers.objective) {
+      case'Add a Department to the Database':
+      addDept();
+      case'Add a Role to the Database':
+      addRole();
+      case'Add an employee to the Database':
+      addEmployee();
+      case'View All Departments in the Database':
+      return(viewDepts())
+      case'View All Roles in the Database':
+      viewRoles();
+      case'View All Employees in the Database':
+      viewEmployees();
+      case'Change the Roles of an Employee':
+      changeRoles();
+      case'QUIT':
+      console.log('Exited');
+      break;
+  };
+})
+.catch( error => {
+  if(error) {
+      console.log(error);
+  }
+})
+
+}
+
+
 connection.connect((err) => {
   if (err) throw err;
-  getDepartments();
+  // viewDepts();
+  askQuestions();
 });
+
+
