a9136def377e5449bcd248c2242035299434fc6e
Joe Malone <joe.d.malone@gmail.com>
2021-04-02 17:37:28 -0500

    Rework of the add Depts function, in order to facilitate results access later in the cli app

--- index.js
+++ index.js
@@ -17,63 +17,126 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
-
-
-const viewDepts = () => {
-    connection.query('SELECT d.name FROM departments d', (err, res) => {
-    if (err) throw err;
-    console.table(res);
-  });
+async function getViewDepts() {
+  const result = await viewDepts();
+  console.table(result)
+  // connection.end();
 }
 
-const addDept = () => {
-  q.prompt([
-    {
-      type: 'input', 
-      name: 'deptName',
-      message: 'What is the name of the department you would like to add'
-    }
-  ])
-  .then((answers) => {
-    let query = `INSERT INTO departments(name) VALUES("${answers.deptName}")`
-    connection.query(query, (err, res) => {
-    if (err) throw err;
-    // console.table(res);
+function viewDepts() {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT d.name FROM departments d', (err, res) => {
+    return err ? reject(err) : resolve(res);
     });
   })
-  .then( function () {
-    let questions = {
-      type: 'list',
-      name: 'objective',
-      message: 'What would you like to do?',
-      choices: [
-        'Add another Department', 
-        'Main Menu',
-        'QUIT'
-      ]
-    }
-    q.prompt(questions).then((answers) => {
-    switch(answers.objective) {
-      case 'Add another Department':
-        return (addDept());
-      case 'Main Menu':
-        return(askQuestions());
-      case 'QUIT':
-        return(connection.end());
-      }
-    })
+}
+
+const addDept = (input) => {
+  return new Promise((resolve, reject) => {
+      let query = `INSERT INTO departments(name) VALUES("${input}")`
+      connection.query(query, (err, res) => {
+      return err ? reject(err) : resolve(res);
+      })
   })
 }
 
+async function getAddDept(input) {
+  const result = await addDept(input);
+  const depts = await viewDepts();
+  // console.table(depts)
+  // connection.end();
+}
 
+const addDeptPrompt = () => {
+  q.prompt([
+      {
+        type: 'input', 
+        name: 'deptName',
+        message: 'What is the name(s) of the department you would like to add'
+      }
+    ])
+    .then((answers) => {
+      getAddDept(answers.deptName);      
+      
+    })
+    .then( function () {
+      let questions = {
+        type: 'list',
+        name: 'objective',
+        message: 'What would you like to do?',
+        choices: [
+          'Add another Department', 
+          'Main Menu',
+          `QUIT 
+          
+          `
+        ]
+      }
+      q.prompt(questions).then((answers) => {
+      switch(answers.objective) {
+        case 'Add another Department':
+          return (addDeptPrompt());
+        case 'Main Menu':
+          return(askQuestions());
+        case 'QUIT':
+          return(connection.end());
+        }
+      })
+    })
+  }
 const viewRoles = () => {
     connection.query('SELECT r.title, r.salary FROM roles r JOIN', (err, res) => {
     if (err) throw err;
     // Log all results of the SELECT statement
     console.table(res);
-    connection.end();
   });
 }
+
+// const addRole = () => {
+// q.prompt([
+//     {
+//       type: 'input', 
+//       name: 'roleName',
+//       message: 'What is the name(s) of the role you would like to add?'
+//     }, 
+//     {
+//       type: 'list',
+//       name: 'deptName',
+//       message: "To which department does the role belong?",
+//       choices: 
+
+//     }
+//   ])
+
+//   .then((answers) => {
+//     let query = `INSERT INTO roles(title, salary, departments_id) VALUES("${answers.roleName}","${answers.salary}", )`
+//     connection.query(query, (err, res) => {
+//       if (err) throw err;
+//     })
+//   })
+//   .then( function () {
+//     let questions = {
+//       type: 'list',
+//       name: 'objective',
+//       message: 'What would you like to do?',
+//       choices: [
+//         'Add another Role', 
+//         'Main Menu',
+//         'QUIT'
+//       ]
+//     }
+//     q.prompt(questions).then((answers) => {
+//     switch(answers.objective) {
+//       case 'Add another Role':
+//         return (addRole());
+//       case 'Main Menu':
+//         return(askQuestions());
+//       case 'QUIT':
+//         return(connection.end());
+//       }
+//     })
+//   })
+// }
 
 const viewEmployees = () => {
     connection.query('SELECT e.first_name, e.last_name,  FROM employees e', (err, res) => {
@@ -105,7 +168,7 @@ function askQuestions() {
   q.prompt(questions).then((answers) => {
     switch(answers.objective) {
       case'Add a Department to the Database':
-      return(addDept());
+      return(addDeptPrompt());
       case'Add a Role to the Database':
       return(addRole());
       case'Add an employee to the Database':
