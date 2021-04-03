a3c7bd2d0ece95dca713b8dd5199f4e8e530b4ee
Joe Malone <joe.d.malone@gmail.com>
2021-04-03 15:25:35 -0500

    re-work of async functions for making prompts appear below tabled data

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const q = require('inquirer');
 const table = require('console.table');
 require('dotenv').config();
 
+
 const connection = mysql.createConnection({
   host: 'localhost',
 
@@ -13,81 +14,103 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
 
   // Be sure to update with your own MySQL password!
-  password: process.env.PASSWORD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
 async function getViewDepts() {
   const result = await viewDepts();
+  console.log("\n")
   console.table(result)
+  // logs();
+  // askQuestions();
+  
   // connection.end();
 }
 
-function viewDepts() {
-  return new Promise((resolve, reject) => {
-    connection.query('SELECT d.name FROM departments d', (err, res) => {
-    return err ? reject(err) : resolve(res);
-    });
+
+// function viewDepts() {
+//   return new Promise((resolve, reject) => {
+//     connection.query('SELECT d.name FROM departments d', (err, res) => {
+//     return err ? reject(err) : resolve(res);
+//     });
+//   })
+// }
+
+// const addDept = (input) => {
+//   return new Promise((resolve, reject) => {
+//       let query = `INSERT INTO departments(name) VALUES("${input}")`
+//       connection.query(query, (err, res) => {
+//       return err ? reject(err) : resolve(res);
+//       })
+//   })
+// }
+
+// async function getAddDept(input) {
+//   const result = await addDept(input)
+//   depts = await viewDepts();
+//   // console.log(depts)
+
+// }
+
+async function addDept(input) {
+  let query = `INSERT INTO departments(name) VALUES("${input}")`
+  connection.query(query, (err, res) => {})
+  depts = await viewDepts();
+  console.table(depts)
+}
+async function viewDepts() {
+  let query = 'SELECT d.name FROM departments d'
+  connection.query(query, async (err, res) => {
+    console.table(res);
+    // console.log('\n');
+    // askQuestions();
+    return(res);
   })
 }
 
-const addDept = (input) => {
-  return new Promise((resolve, reject) => {
-      let query = `INSERT INTO departments(name) VALUES("${input}")`
-      connection.query(query, (err, res) => {
-      return err ? reject(err) : resolve(res);
-      })
-  })
-}
-
-async function getAddDept(input) {
-  const result = await addDept(input);
-  const depts = await viewDepts();
-  // console.table(depts)
-  // connection.end();
-}
-
-const addDeptPrompt = () => {
-  q.prompt([
+const addDeptPrompt = async () => {
+ await q.prompt([
       {
         type: 'input', 
         name: 'deptName',
         message: 'What is the name(s) of the department you would like to add'
       }
     ])
-    .then((answers) => {
-      getAddDept(answers.deptName);      
-      
-    })
-    .then( function () {
-      let questions = {
-        type: 'list',
-        name: 'objective',
-        message: 'What would you like to do?',
-        choices: [
-          'Add another Department', 
-          'Main Menu',
-          `QUIT 
-          
-          `
-        ]
+  .then(async(answers) => {
+    let departments = await addDept(answers.deptName);
+    console.table(departments);
+    console.log("\n");
+  })
+  .then( async function () {
+    let questions = {
+      type: 'list',
+      name: 'objective',
+      message: 'What would you like to do?',
+      choices: [
+        'Add another Department', 
+        'Main Menu',
+        `QUIT 
+        
+        `
+      ]
+    }
+    q.prompt(questions).then((answers) => {
+    switch(answers.objective) {
+      case 'Add another Department':
+        return (addDeptPrompt());
+      case 'Main Menu':
+        return(askQuestions());
+      case 'QUIT':
+        return(connection.end());
       }
-      q.prompt(questions).then((answers) => {
-      switch(answers.objective) {
-        case 'Add another Department':
-          return (addDeptPrompt());
-        case 'Main Menu':
-          return(askQuestions());
-        case 'QUIT':
-          return(connection.end());
-        }
-      })
     })
-  }
-const viewRoles = () => {
-    connection.query('SELECT r.title, r.salary FROM roles r JOIN', (err, res) => {
+  })
+}
+const viewRoles = async () => {
+    let query = 'SELECT r.title, r.salary FROM roles r JOIN'
+  await  connection.query(query, (err, res) => {
     if (err) throw err;
-    // Log all results of the SELECT statement
     console.table(res);
   });
 }
@@ -147,34 +170,34 @@ const viewEmployees = () => {
   });
 }
 
-function askQuestions() {
+async function askQuestions() {
 
   let questions = {
     type: 'list',
     name: 'objective',
     message: 'What would you like to do?',
     choices: [ 
-      "Add a Department to the Database", 
-      "Add a Role to the Database", 
-      "Add an Employee to the Database", 
-      "View All Departments in the Database", 
+      "View All Departments in the Database",
+      "Add a Department to the Database",  
       "View All Roles in the Database", 
+      "Add a Role to the Database", 
       "View All Employees in the Database",
+      "Add an Employee to the Database",
       "Change the Roles of an Employee", 
       "QUIT"
     ]
   }
 
-  q.prompt(questions).then((answers) => {
+  await q.prompt(questions).then((answers) => {
     switch(answers.objective) {
       case'Add a Department to the Database':
       return(addDeptPrompt());
+      case'View All Departments in the Database':
+      return(viewDepts())
       case'Add a Role to the Database':
       return(addRole());
       case'Add an employee to the Database':
       return(addEmployee());
-      case'View All Departments in the Database':
-      return(viewDepts())
       case'View All Roles in the Database':
       return(viewRoles());
       case'View All Employees in the Database':
